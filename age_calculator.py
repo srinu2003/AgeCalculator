4781246c0edfc2083116d4eb45f7cc9e5d31cba7
srinu2003 <tsrin2003@gmail.com>
2023-01-13 19:47:41 +0530

    2nd

--- age_calculator.py
+++ age_calculator.py
@@ -1,5 +1,23 @@
 from datetime import date#,datetime
-
+#dictionary months
+'''
+def leap(year):
+    year=12
+    pass
+month={
+    January : 31,
+    February : 29 if leap(year) else 28,
+    March : 31,
+    April : 30, 
+    May : 31, 
+    June : 30, 
+    July : 31, 
+    August : 31, 
+    September : 30, 
+    October : 31,
+    November : 30, 
+    December : 31,
+}'''
 # date object of today's date
 today = date.today()
 
