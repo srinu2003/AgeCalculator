be5551fec92f466fd728ee4163a8bf71faa251c6
srinu2003 <tsrin2003@gmail.com>
2023-01-13 19:03:22 +0530

    1st commit

--- age_calculator.py
+++ age_calculator.py
@@ -1,18 +1,37 @@
 from datetime import date#,datetime
 
 # date object of today's date
-today = date.today() 
+today = date.today()
 
 print("Current year:", today.year)
 print("Current month:", today.month)
 print("Current day:", today.day)
-# dob = datetime.date(int(input("Enter year:")),int(input( "Enter month:")), int(input("Enter date:")))
-dob = date(2020,10,1)
-print(dob)
-print("Your birthday is on:",dob.strftime("%B %d, %Y"))
-# get the current date and time
-age = today  - dob 
 print(today)
-print(age)
+print("Simple Age Calculator")
 
+# asking user to enter birth year, birth month, and birth date
+birthYear = int(input("Enter the birth year: "))
+birthMonth = int(input("Enter the birth month: "))
+birthDay = int(input("Enter the birth day: "))
 
+# converting integer values to date format using the date() method
+dateOfBirth = date(birthYear, birthMonth, birthDay)  
+# dob = datetime.date(int(input("Enter year:")),int(input( "Enter month:")), int(input("Enter date:")))
+print("Your Date of birth is:",dateOfBirth.strftime("%d %B, %Y"))
+
+# a bool representing if today's day, or month precedes the birth day, or month
+day_check = ((today.month, today.day) < (dateOfBirth.month, dateOfBirth.day))
+
+# calculating the difference between the current year and birth year
+year_diff  = today.year - dateOfBirth.year
+
+age_in_years = year_diff - day_check  
+
+# calculating the remaining months
+remaining_months = abs(today.month - dateOfBirth.month)
+
+# calculating the remaining days
+remaining_days = abs(today.day - dateOfBirth.day)
+
+# printing the age for the users  
+print("Age:", age_in_years, "Years", remaining_months, "Months and", remaining_days, "days")
